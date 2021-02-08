4bfb280735c7c20ffe4ba323914cee9131d89c30
Vu Long Tran <vulongtran@users.noreply.github.com>
2021-02-08 21:33:16 +0800

    new base repo

---
+++ financialnewsapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FinancialnewsappConfig(AppConfig):
+    name = 'financialnewsapp'
---
+++ financialnewsapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from newsapi import NewsApiClient
+import json
+# Initializing this django app with a https://newsapi.org/ api key which newsapi offers for one month free to test with. 
+newsapi = NewsApiClient(api_key='APIkeyhere')
+all_articles = newsapi.get_everything(q='bitcoin',sources='yahoo,the-verge,cnbc,bbc-news',
+language='en',sort_by='relevancy')
+
+# loading all_articles as json
+new = json.dumps(all_articles)
+# This will allow us to create creat dictionary from the json which will make easier for us to use the data
+data = json.loads(new)
+def index(request):
+    return render(request,'news/index.html',{'data':data})
